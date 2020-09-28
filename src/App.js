0b4de737b5ec6f04c26a33fc01e8a440f23a4576
Nikhil Singh <singhyesh2000@gmail.com>
2020-09-28 17:56:21 +0530

    Single Page Applications Part 1

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div >
+        <div className="App">
           <Main />
         </div>
       </BrowserRouter>
--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,31 +1,30 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron } from 'reactstrap';
+import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
-
     constructor(props) {
         super(props);
+
+        this.toggleNav = this.toggleNav.bind(this);
         this.state = {
             isNavOpen: false
         };
-        this.toggleNav = this.toggleNav.bind(this);
     }
+
     toggleNav() {
         this.setState({
             isNavOpen: !this.state.isNavOpen
         });
     }
+
     render() {
         return (
-            //React.fragment short form <>
-            <>
+            <div>
                 <Navbar dark expand="md">
-                    <div className="container" >
-                        <NavbarToggler onClick={"this.toggleNav"} />
-                        <NavbarBrand className="mr-auto" href="/" >
-                            <img src='assets/images/logo.png' height="30" width="41" alt='Ristorante Con Fusion' />
-                        </NavbarBrand>
+                    <div className="container">
+                        <NavbarToggler onClick={this.toggleNav} />
+                        <NavbarBrand className="mr-auto" href="/"><img src='assets/images/logo.png' height="30" width="41" alt='Ristorante Con Fusion' /></NavbarBrand>
                         <Collapse isOpen={this.state.isNavOpen} navbar>
                             <Nav navbar>
                                 <NavItem>
@@ -45,7 +44,7 @@ class Header extends Component {
                     </div>
                 </Navbar>
                 <Jumbotron>
-                    <div className="container" >
+                    <div className="container">
                         <div className="row row-header">
                             <div className="col-12 col-sm-6">
                                 <h1>Ristorante con Fusion</h1>
@@ -54,7 +53,7 @@ class Header extends Component {
                         </div>
                     </div>
                 </Jumbotron>
-            </>
+            </div>
         );
     }
 }
